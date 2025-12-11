6b942324603118a608cc7268d62f6b5da31bfd2f
CascallenSteve <cascallensteve@gmail.com>
2025-12-11 13:59:18 +0300

    commited

--- product/serializers.py
+++ product/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
-from .models import Product
+from .models import Product, Category
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(write_only=True, required=True)
+    category = serializers.CharField(source="category.name", read_only=True)
+
     class Meta:
         model = Product
         fields = [
@@ -12,6 +15,7 @@ class ProductSerializer(serializers.ModelSerializer):
             "description",
             "price",
             "amount",
+            "category_name",
             "category",
             "is_available",
             "demand",
@@ -19,4 +23,17 @@ class ProductSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
         ]
-        read_only_fields = ["id", "created_at", "updated_at"]
+        read_only_fields = ["id", "created_at", "updated_at", "category"]
+
+    def create(self, validated_data):
+        category_name = validated_data.pop("category_name")
+        category, _ = Category.objects.get_or_create(name=category_name)
+        validated_data["category"] = category
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        category_name = validated_data.pop("category_name", None)
+        if category_name is not None:
+            category, _ = Category.objects.get_or_create(name=category_name)
+            validated_data["category"] = category
+        return super().update(instance, validated_data)
